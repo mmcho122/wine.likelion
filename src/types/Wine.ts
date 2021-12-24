aad1846937832f6322a497557a5223bb0cdd2be6
mijin <mmcho122@gmail.com>
2021-12-24 21:05:00 +0900

    :sparkles: API 연동

---
+++ src/types/Wine.ts
@@ -0,0 +1,10 @@
+import { Rating } from "./Rating";
+
+export interface Wine {
+    winery: string,
+    wine: string,
+    rating: Rating,
+    location: string,
+    image: string,
+    id: number,
+  }
